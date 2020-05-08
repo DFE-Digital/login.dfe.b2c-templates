b037f8c30fd53bb9f0c89f6b0aa7e606352a6236
Aman Gupta <aman.gupa-ext@ab-inbev.com>
2020-05-08 23:46:37 +0530

    UI and function changes

--- src/B2CAzureFunc/SignupConfirmation.cs
+++ src/B2CAzureFunc/SignupConfirmation.cs
@@ -23,15 +23,19 @@ namespace B2CAzureFunc
             try
             {
                 log.LogInformation("Request started");
+
+
                 string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
                 dynamic data = JsonConvert.DeserializeObject(requestBody);
                 log.LogInformation(requestBody);
                 var accountActivationEmailExpiryInSeconds = Convert.ToInt32(Environment.GetEnvironmentVariable("AccountActivationEmailExpiryInSeconds", EnvironmentVariableTarget.Process));
 
-                string token = TokenBuilder.BuildIdToken(data.email.ToString(), DateTime.UtcNow.AddSeconds(accountActivationEmailExpiryInSeconds), req.Scheme, req.Host.Value, req.PathBase.Value,data.ObjectId.ToString());
+
+                string token = TokenBuilder.BuildIdToken(data.email.ToString(), DateTime.UtcNow.AddSeconds(accountActivationEmailExpiryInSeconds), req.Scheme, req.Host.Value, req.PathBase.Value, data.ObjectId.ToString());
+
                 string b2cURL = Environment.GetEnvironmentVariable("B2CAuthorizationUrl", EnvironmentVariableTarget.Process);
                 string b2cTenant = Environment.GetEnvironmentVariable("B2CTenant", EnvironmentVariableTarget.Process);
-                string b2cPolicyId = Environment.GetEnvironmentVariable("B2CSignupConfirmPolicy", EnvironmentVariableTarget.Process);
+                string b2cPolicyId = Environment.GetEnvironmentVariable("B2CSignupConirmPolicy", EnvironmentVariableTarget.Process);
                 string b2cClientId = Environment.GetEnvironmentVariable("B2CClientId", EnvironmentVariableTarget.Process);
                 string b2cRedirectUri = Environment.GetEnvironmentVariable("B2CRedirectUri", EnvironmentVariableTarget.Process);
                 string url = URLBuilder.BuildUrl(token, b2cURL, b2cTenant, b2cPolicyId, b2cClientId, b2cRedirectUri);
