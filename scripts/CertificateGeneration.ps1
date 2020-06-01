a0f905bf583654064c7c7d39241a54b93a194e60
Paul <paul@phobos-software.net>
2020-06-01 20:48:31 +0100

    check: upload schanges

--- scripts/CertificateGeneration.ps1
+++ scripts/CertificateGeneration.ps1
@@ -1,17 +1,14 @@
 # pfx certification stored path
 $certroopath = "C:\DFETemp"
-
 # Certification name - should match with FQDN of Windows Azure creating VM
 $certname = "oat-api-customerengagement.platform.education.gov.uk"
-
 # Certification password - should be match with password of Windows Azure creating VM
 $certpassword = "Secret@123"
-
-$cert=New-SelfSignedCertificate -DnsName "$certname" -CertStoreLocation cert:\LocalMachine\My -Subject 'cn=oat-api-customerengagement.platform.education.gov.uk' -Type Custom -KeySpec Signature -KeyExportPolicy Exportable -HashAlgorithm sha256 -KeyLength 4096 -KeyUsageProperty Sign -KeyUsage CertSign
+$cert = New-SelfSignedCertificate -DnsName "$certname" -CertStoreLocation cert:\LocalMachine\My -Subject 'cn=oat-api-customerengagement.platform.education.gov.uk' -Type Custom -KeySpec Signature -KeyExportPolicy Exportable -HashAlgorithm sha256 -KeyLength 4096 -KeyUsageProperty Sign -KeyUsage CertSign
 
 # Generate certificates from root (For Client Authentication only) (Not for web server)
 
-$cert1=New-SelfSignedCertificate -Type Custom -KeySpec Signature -Subject 'cn=oat-api-customerengagement.platform.education.gov.uk' -HashAlgorithm sha256 -KeyLength 2048 -CertStoreLocation cert:\LocalMachine\My -Signer $cert -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2")
+$cert1 = New-SelfSignedCertificate -Type Custom -KeySpec Signature -Subject 'cn=oat-api-customerengagement.platform.education.gov.uk' -HashAlgorithm sha256 -KeyLength 2048 -CertStoreLocation cert:\LocalMachine\My -Signer $cert -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2")
 
 $pwd = ConvertTo-SecureString -String $certpassword -Force -AsPlainText
 
@@ -23,5 +20,5 @@ Export-PfxCertificate -cert $certwithThumb -FilePath $filepath -Password $pwd
 
 'Thumpprint: '+$cert1.Thumbprint
 
-Remove-Item -Path $certwithThumb
-Remove-Item -Path $certwithThumb
+# Remove-Item -Path $certwithThumb
+# Remove-Item -Path $certwithThumb
--- scripts/UploadFrameworkPolicy.ps1
+++ scripts/UploadFrameworkPolicy.ps1
@@ -14,8 +14,8 @@ Param(
     [Parameter(Mandatory = $true)][string]$ClientID,
     [Parameter(Mandatory = $true)][string]$ClientSecret,
     [Parameter(Mandatory = $true)][string]$TenantId,
-    [Parameter(Mandatory = $true)][string]$PolicyId,
-    [Parameter(Mandatory = $true)][string]$PathToFile
+    [Parameter(Mandatory = $true)][string[]]$PolicyNames,
+    [Parameter(Mandatory = $true)][string[]]$FilePaths
 )
 
 try {
@@ -28,9 +28,12 @@ try {
     $headers.Add("Content-Type", 'application/xml')
     $headers.Add("Authorization", 'Bearer ' + $token)
 
-    $graphuri = 'https://graph.microsoft.com/beta/trustframework/policies/' + $PolicyId + '/$value'
-    $policycontent = Get-Content $PathToFile
-    $response = Invoke-RestMethod -Uri $graphuri -Method Put -Body $policycontent -Headers $headers
+
+    for ($i=0; $i -eq $PolicyNames.Length; $i++) {
+        $graphuri = 'https://graph.microsoft.com/beta/trustframework/policies/' + $PolicyNames[$i] + '/$value'
+        $policycontent = Get-Content $FilePaths[$i]
+        $response = Invoke-RestMethod -Uri $graphuri -Method Put -Body $policycontent -Headers $headers
+    }
 
     Write-Host "Policy" $PolicyId "uploaded successfully."
 }
