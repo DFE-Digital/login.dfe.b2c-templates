5046a6c97364a0aac19102c7b2abdd252a2c4d90
Paul <paul@phobos-software.net>
2020-06-02 13:22:00 +0100

    fix: templates

--- test.ps1
+++ test.ps1
@@ -3,14 +3,46 @@
 # $policyPath = $PSScriptRoot + "\policy\TrustFrameworkBase.xml"
 # Invoke-Command -ScriptBlock $scriptBlock -ArgumentList ($args + @('456313e4-5edf-499b-b3a4-ab54e79c22d4', '8_bDl1v-6b6N1IpaY.YCdCOSctltpWz~Ph ', 'ce495425-4863-4e46-aa86-4a4e5a5bac0d', 'B2C_1A_TrustFrameworkBase_invitation', $policyPath))
 
+$tenantId = "ce495425-4863-4e46-aa86-4a4e5a5bac0d"
+$clientId = "456313e4-5edf-499b-b3a4-ab54e79c22d4"
+$secret = "8_bDl1v-6b6N1IpaY.YCdCOSctltpWz~Ph"
+$scriptPath = ".\scripts\UploadFrameworkPolicy.ps1"
 
+$policyId = "B2C_1A_TrustFrameworkBase_Invitation"
+$policyFile = ".\policy\TrustFrameworkBase_Invitation.xml"
+Invoke-Expression "$scriptPath $clientId $secret $tenantId $policyId $policyFile"
 
-$scriptPath = ".\$($env:BUILD_SOURCESDIRECTORY)\scripts\UploadFrameworkPolicy.ps1"
+$policyId = "B2C_1A_TrustFrameworkExtensions_Invitation"
+$policyFile = "$($env:BUILD_SOURCESDIRECTORY)\policy\TrustFrameworkBase_Invitation.xml"
+Invoke-Expression "$scriptPath $clientId $secret $tenantId $policyId $policyFile"
 
-Write-Host $scriptPath
-$dave = "d"
+$policyId = "B2C_1A_Account_Signup"
+$policyFile = "$($env:BUILD_SOURCESDIRECTORY)\policy\Account_Signup.xml"
+Invoke-Expression "$scriptPath $clientId $secret $tenantId $policyId $policyFile"
 
-Invoke-Expression "$scriptPath $dave"
+$policyId = "B2C_1A_Find_Email"
+$policyFile = "$($env:BUILD_SOURCESDIRECTORY)\policy\Find_Email.xml"
+Invoke-Expression "$scriptPath $clientId $secret $tenantId $policyId $policyFile"
+
+$policyId = "B2C_1A_Password_Reset_Conformation"
+$policyFile = "$($env:BUILD_SOURCESDIRECTORY)\policy\Password_Reset_Confirmation.xml"
+Invoke-Expression "$scriptPath $clientId $secret $tenantId $policyId $policyFile"
+
+$policyId = "B2C_1A_Password_Reset"
+$policyFile = "$($env:BUILD_SOURCESDIRECTORY)\policy\Password_Reset.xml"
+Invoke-Expression "$scriptPath $clientId $secret $tenantId $policyId $policyFile"
+
+$policyId = "B2C_1A_Signin_Invitation"
+$policyFile = "$($env:BUILD_SOURCESDIRECTORY)\policy\Signin_Invitation.xml"
+Invoke-Expression "$scriptPath $clientId $secret $tenantId $policyId $policyFile"
+
+$policyId = "B2C_1A_Signup_Confirmation"
+$policyFile = "$($env:BUILD_SOURCESDIRECTORY)\policy\Signup_Confirmation.xml"
+Invoke-Expression "$scriptPath $clientId $secret $tenantId $policyId $policyFile"
+
+$policyId = "B2C_1A_Signup_Invitation"
+$policyFile = "$($env:BUILD_SOURCESDIRECTORY)\policy\Signup_Invitation.xml"
+Invoke-Expression "$scriptPath $clientId $secret $tenantId $policyId $policyFile"
 
 ##ADO-PipelineManagement
 
